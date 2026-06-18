44209f5b2319b2b18abaaf0a411d09021784d9f4
Qamil <dreamtech@MacBook-Air-QH.bbrouter>
2026-06-18 17:52:32 +0200

    Modify vehicleModel to combine tables to provide other data

--- backend/models/vehicleModel.js
+++ backend/models/vehicleModel.js
@@ -13,13 +13,25 @@ const vehicleModel = {
     }
     ,
     getAllVehicles: async () => {
-        return await db("vehicle").select("*")
+        return await db("vehicle as v")
+                        .join("brand as b","v.brand_id","b.brand_id")
+                        .join("vehicle_cost as vc","v.cost_id","vc.vcost_id")
+                        .select( "v.*",
+                                "b.brand",
+                                "vc.cost_per_hour",
+                                "vc.cost_per_day")
     },
 
     getVehicleById: async (vehicle_id) => {
-        return await db("vehicle")
-            .where({vehicle_id})
-            .first()
+        return await db("vehicle as v")
+                    .join("brand as b","v.brand_id","b.brand_id")
+                    .join("vehicle_cost as vc","v.cost_id","vc.vcost_id")
+                    .select("v.*",
+                            "b.brand",
+                            "vc.cost_per_hour",
+                            "vc.cost_per_day")
+                    .where("v.vehicle_id",vehicle_id)
+                    .first()
     },
 
     updateVehicle: async(vehicle_id,data) => {
