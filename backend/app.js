9a27053546c4fc2e0d8e1d48252d91818e35b232
Qamil <dreamtech@MacBook-Air-QH.bbrouter>
2026-06-23 22:05:41 +0200

    Add general limiter and use helmet package

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from "express"
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import helmet from "helmet"
 import vehicleRouter from "./routes/vehicleRoute.js";
 import clientRouter from "./routes/clientRoute.js";
 import vehicleMaintenanceRouter from "./routes/vehicleMaintenanceRoute.js";
@@ -14,20 +15,33 @@ import paymentRouter from "./routes/paymentRoute.js";
 import authRouter from "./routes/authRoute.js";
 import brandRouter from "./routes/brandRoute.js";
 import authMiddleware from "./middleware/authMiddleware.js";
+import {generalLimiter} from "./middleware/rateLimit.js";
 
 
 const app = express()
 const PORT = 5005
-
+console.log(process.env.CLIENT_URL)
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-      origin:"http://localhost:5173",
+      origin:process.env.CLIENT_URL,
       credentials:true
 }))
+ app.use(helmet({
+      crossOriginResourcePolicy: {
+            policy: "cross-origin",
+    }
+ }));
+app.use(generalLimiter)
+
+app.use((error,req,res,next) => {
+      res.status(500).json({
+            message: "Internal server error"
+      })
+})
 
 app.use("/api/auth",authRouter);
-app.use("/api/brands",authMiddleware,brandRouter)
+app.use("/api/brands",authMiddleware,generalLimiter,brandRouter)
 app.use("/api/vehicles",authMiddleware,vehicleRouter)
 app.use("/api/clients",authMiddleware,clientRouter)
 app.use("/api/vehicleMaintenances",authMiddleware,vehicleMaintenanceRouter)
--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -9,3 +9,13 @@ export const loginLimiter = rateLimit({
     standardHeaders: true,
     legacyHeaders:false
 })
+
+export const generalLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 300,
+    message: {
+        message : "Too many login attempts. Try again later."
+    },
+    standardHeaders: true,
+    legacyHeaders: false
+})
